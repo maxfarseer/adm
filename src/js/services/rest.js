86109bccfb48fca2ea5e197f1cbdf2d8b4d300fc
Max Patsiansky <m.patsianskiy@zmeke.com>
2014-10-24 17:53:10 +0400

    work with signup

--- src/js/services/rest.js
+++ src/js/services/rest.js
@@ -3,13 +3,17 @@
  * @ngInject
  */
 function restService($resource) {
+  var rootLink = 'http://localhost:8001/builds/development';
   var rest = {
       test: $resource('http://api.openweathermap.org/data/2.5/weather?q=London,uk', {}, {
         load: {method: 'GET'}
       }),
       git: $resource('https://api.github.com/users', {}, {
         load: {method: 'GET', isArray: true}
-      })
+      }),
+      signup: $resource('/api/signup', {}, {
+        load: {method: 'GET'}
+      }),
     };
 
   return rest;
